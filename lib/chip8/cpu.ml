51c36748fef94a6d6ad5ab09ce7e274d461b8af9
Antonis Kalou <kalouantonis@protonmail.com>
2023-12-22 12:33:27 +0200

    Support 8XY6, 8XYE

--- lib/chip8/cpu.ml
+++ lib/chip8/cpu.ml
@@ -45,6 +45,8 @@ type instruction =
 | Add_vx_to_vy of register * register
 | Subtract_vy_from_vx of register * register
 | Subtract_vx_from_vy of register * register
+| Shift_right of register * register
+| Shift_left of register * register
 | Binary_or of register * register
 | Binary_and of register * register
 | Binary_xor of register * register
@@ -74,7 +76,9 @@ let decode opcode =
   | (0x8, x, y, 0x3) -> Binary_xor (Uint8.of_int x, Uint8.of_int y)
   | (0x8, x, y, 0x4) -> Add_vx_to_vy (Uint8.of_int x, Uint8.of_int y)
   | (0x8, x, y, 0x5) -> Subtract_vy_from_vx (Uint8.of_int x, Uint8.of_int y)
+  | (0x8, x, y, 0x6) -> Shift_right (Uint8.of_int x, Uint8.of_int y)
   | (0x8, x, y, 0x7) -> Subtract_vx_from_vy (Uint8.of_int x, Uint8.of_int y)
+  | (0x8, x, y, 0xE) -> Shift_left (Uint8.of_int x, Uint8.of_int y)
   | (0x9, x, y, 0x0) -> Skip_if_vx_vy_ne (Uint8.of_int x, Uint8.of_int y)
   | (0xA, n1, n2, n3) -> Set_index (Nibbles.to_uint16 n1 n2 n3)
   | (0xB, n1, n2, n3) -> Jump0 (Nibbles.to_uint16 n1 n2 n3)
@@ -114,6 +118,14 @@ let execute t instruction =
     if x < y
     then t.registers.(0xF) <- Uint8.one
     else t.registers.(0xF) <- Uint8.zero
+  | Shift_right (vx, vy) ->
+    let y = t.registers.(Uint8.to_int vy) in
+    t.registers.(Uint8.to_int vx) <- Uint8.shift_right y 1;
+    t.registers.(0xF) <- Uint8.logand y Uint8.one;
+  | Shift_left (vx, vy) ->
+    let y = t.registers.(Uint8.to_int vy) in
+    t.registers.(Uint8.to_int vx) <- Uint8.shift_left y 1;
+    t.registers.(0xF) <- Uint8.logand y (Uint8.of_int 0b1000_0000);
   | Binary_or (vx, vy) ->
     let x = t.registers.(Uint8.to_int vx) in
     let y = t.registers.(Uint8.to_int vy) in
