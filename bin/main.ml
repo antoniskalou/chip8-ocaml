555e713d0a07536159c17419d84eda37849e33a0
Antonis Kalou <kalouantonis@protonmail.com>
2024-01-04 18:19:39 +0200

    Allow specifying BG and FG colour with cmdline

--- bin/main.ml
+++ bin/main.ml
@@ -2,25 +2,26 @@ open Tsdl
 open Stdint
 open Chip8
 
-(* module Emulator = struct
-  type config =
-    { render_scale : int
-    ; bg_color : uint32
-    ; fg_color : uint32
-    }
-
-  type t =
-    { cpu : Cpu.t
-    ; memory : Memory.t
-    }
-end *)
-
-let render_scale = 20
 let target_fps = 60
 let target_mhz = 540
 (* the number of cycles that run before a refresh occurs *)
 let cycles_per_refresh = target_mhz / target_fps
 let refresh_per_second = (1. /. Float.of_int target_fps)
+
+type config =
+  { render_scale : int (* TODO: make explicit width *)
+  ; bg_color : uint32
+  ; fg_color : uint32
+  }
+
+let rgba_of_uint32 i =
+  let i = Uint32.to_int i in
+  (
+    (i lsr 16) land 0xFF, (* r *)
+    (i lsr 8) land 0xFF,  (* g *)
+    i land 0xFF,          (* b *)
+    0xFF
+  )
 
 let or_exit =
   function
@@ -30,23 +31,27 @@ let or_exit =
 let init_sdl2 () =
   Sdl.init Sdl.Init.(video + events + audio) |> or_exit
 
-let init_graphics () =
+let init_graphics config =
   let w =
     Sdl.create_window
-      ~w:(64 * render_scale) ~h:(32 * render_scale)
+      ~w:(64 * config.render_scale) ~h:(32 * config.render_scale)
       "Chip8 (Vulkan)" Sdl.Window.vulkan
     |> or_exit
   in
   let renderer = Sdl.create_renderer w ~index:(-1) |> or_exit in
+  (* render to white first. we know if its visible the screen was never cleared or
+    rendered to. *)
   Sdl.set_render_draw_color renderer 0xFF 0xFF 0xFF 0xFF |> or_exit;
   renderer
 
-let clear_graphics renderer =
-  Sdl.set_render_draw_color renderer 0x00 0x00 0x00 0xFF |> or_exit;
+let clear_graphics { bg_color; _ } renderer =
+  let (r, g, b, a) = rgba_of_uint32 bg_color in
+  Sdl.set_render_draw_color renderer r g b a |> or_exit;
   Sdl.render_clear renderer |> or_exit
 
-let draw_graphics buffer renderer =
-  Sdl.set_render_draw_color renderer 0x00 0xFF 0x00 0xFF |> or_exit;
+let draw_graphics { fg_color; render_scale; _ } buffer renderer =
+  let (r, g, b, a) = rgba_of_uint32 fg_color in
+  Sdl.set_render_draw_color renderer r g b a |> or_exit;
   buffer |> Array.iteri (fun i b ->
     if b then begin
       (* there are probably more efficient ways to do this rather
@@ -129,21 +134,35 @@ let render_scale = ref 1
 let anon_fun path = rom_file := path
 
 let speclist =
-  [("-fg", Arg.Set_string fg_color, "Set the foreground render colour (uses hex format)");
-   ("-bg", Arg.Set_string bg_color, "Set the background render colour (uses hex format)");
+  [("-fg", Arg.Set_string fg_color, "Set the foreground render colour (uses 0x format)");
+   ("-bg", Arg.Set_string bg_color, "Set the background render colour (uses 0x format)");
    ("-scale", Arg.Set_int render_scale, "Set the render scale, defaults to 64x32 (original resolution)")]
+
+let bad_arg msg =
+  Printf.eprintf "ERROR: %s\n\n" msg;
+  Arg.usage speclist usage_msg;
+  exit 2
 
 let parse_args () =
   Arg.parse speclist anon_fun usage_msg;
-  if String.equal String.empty !rom_file then begin
-    Printf.eprintf "ERROR: ROM file not provided!\n\n";
-    Arg.usage speclist usage_msg;
-    exit 2
-  end
+  (* validate rom file param *)
+  if String.equal String.empty !rom_file then
+    bad_arg "ROM file not provided!";
+  (* validate scale param *)
+  if !render_scale <= 0 then
+    bad_arg "invalid render scale, must be > 0";
+  try
+    { render_scale = !render_scale
+    ; bg_color = Uint32.of_string !bg_color
+    ; fg_color = Uint32.of_string !fg_color
+    }
+  with Invalid_argument _ ->
+    bad_arg "Invalid colour provided, must be in the 0x format"
 
 let () =
-  parse_args ();
+  let config = parse_args () in
   print_license ();
+
   init_sdl2 ();
   let rom = Rom.load !rom_file in
   let memory = Memory.create () in
@@ -151,10 +170,10 @@ let () =
   Memory.load memory ~src:rom ~pos:Memory.rom_base_address;
   let cpu = Cpu.create memory in
   let buzzer = Buzzer.create { volume = 0.2; frequency = 200. } |> or_exit in
-  let renderer = init_graphics () in
+  let renderer = init_graphics config in
   while true do
     handle_event cpu;
-    clear_graphics renderer;
+    clear_graphics config renderer;
     Cpu.update_timers cpu;
     let elapsed =
       Util.timed (fun () ->
@@ -165,6 +184,6 @@ let () =
     if Cpu.sound_playing cpu
     then Buzzer.play buzzer
     else Buzzer.pause buzzer;
-    draw_graphics (Cpu.screen_buffer cpu) renderer;
+    draw_graphics config (Cpu.screen_buffer cpu) renderer;
     Sdl.delay (seconds_to_ms (refresh_per_second -. elapsed))
   done
