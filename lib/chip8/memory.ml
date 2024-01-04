8f1f3d875aa19568ee58c2f4efe14b67316c746e
Antonis Kalou <kalouantonis@protonmail.com>
2024-01-04 19:58:35 +0200

    Remove unused types from memory.ml

--- lib/chip8/memory.ml
+++ lib/chip8/memory.ml
@@ -4,22 +4,22 @@ type t = bytes
 
 let rom_base_address = Uint16.of_int 0x200
 
-let create () : t = Bytes.create 4096
+let create () = Bytes.create 4096
 let of_bytes b = b
 
-let size (t: t): int = Bytes.length t
+let size t = Bytes.length t
 
-let load (t: t) ~src ~pos =
+let load t ~src ~pos =
   Bytes.blit src 0 t (Uint16.to_int pos) (Bytes.length src)
 
-let read_uint16 (t: t) ~(pos: uint16): uint16 =
+let read_uint16 t ~pos =
   Uint16.of_bytes_big_endian t (Uint16.to_int pos)
 
-let read_uint8 (t: t) ~(pos: uint16): uint8 =
+let read_uint8 t ~pos =
   Uint8.of_bytes_big_endian t (Uint16.to_int pos)
 
-let write_uint16 (t: t) ~(pos: uint16) (x: uint16): unit =
+let write_uint16 t ~pos x =
   Uint16.to_bytes_big_endian x t (Uint16.to_int pos)
 
-let write_uint8 (t: t) ~(pos: uint16) (x: uint8): unit =
+let write_uint8 t ~pos x =
   Uint8.to_bytes_big_endian x t (Uint16.to_int pos)
